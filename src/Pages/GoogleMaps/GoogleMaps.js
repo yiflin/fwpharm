5c9fee72acbf84ef020eab5231cffb78c36eea27
yiflin <yifanlin1998@gmail.com>
2019-12-30 11:31:40 -0500

    Update GoogleMaps.js

--- src/Pages/GoogleMaps/GoogleMaps.js
+++ src/Pages/GoogleMaps/GoogleMaps.js
@@ -15,7 +15,7 @@ class GoogleMaps extends Component {
             <div>
                 <MyMapComponent
                 isMarkerShown
-                googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+                googleMapURL=
                 loadingElement={
                 <div style={{ height: `100%` }} />}
                 containerElement={
